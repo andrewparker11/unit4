11c251215d2c93fb6e1f93402ac5514a16b7591f
andrewparker11 <31515304+andrewparker11@users.noreply.github.com>
2017-10-16 14:53:16 -0400

    Updated from Brython Server: 10/16/2017 2:53:15 PM

--- vprint.py
+++ vprint.py
@@ -1,1 +1,12 @@
+#Andrew Parker
+#10/16/17
+#rectangle.py - calculates rectangle area and perimeter
 
+def cd(n):
+    i=n
+    while i>0:
+        print(i)
+        i = i -1
+    print('BOOM! ')
+    
+cd(5)
