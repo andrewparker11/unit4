c763b3164396f8684e005b5dac306acdabca4b8f
andrewparker11 <31515304+andrewparker11@users.noreply.github.com>
2017-10-16 14:27:11 -0400

    Updated from Brython Server: 10/16/2017 2:27:11 PM

--- functionDemo.py
+++ functionDemo.py
@@ -16,3 +16,8 @@ def bigger(num1, num2): #prints which number is bigger
 bigger(13,27) #test1
 bigger(-10,-15) #test2
 bigger('Smeds','Gallino') #bigger in terms of later alphabetical order
+
+def slope(x1,y1,x2,y2):#calculate slope
+    print((y2-y1)/(x2-x1))
+    
+slope(1,2,3,4)
