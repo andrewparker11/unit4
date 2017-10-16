ea013a5b7111e59b9762942aa0a69947b4c791bc
andrewparker11 <31515304+andrewparker11@users.noreply.github.com>
2017-10-16 14:32:51 -0400

    Updated from Brython Server: 10/16/2017 2:32:50 PM

--- rectangle.py
+++ rectangle.py
@@ -2,22 +2,8 @@
 #10/16/17
 #functionDemo.py - learning functions
 
-def hw():
-    print('Hello, world!')
-
-hw() #actually runs the function
-
-def bigger(num1, num2): #prints which number is bigger
-    if num1 > num2:
-        print(num1)
-    else:
-        print(num2)
-        
-bigger(13,27) #test1
-bigger(-10,-15) #test2
-bigger('Smeds','Gallino') #bigger in terms of later alphabetical order
-
-def slope(x1,y1,x2,y2):#calculate slope
-    print((y2-y1)/(x2-x1))
+def rect(l,w):
+    print('Area',= l*w)
+    print('Perimeter',= (2*l)+(2*w))
     
-slope(1,2,2,4)
+rect(3,4)
