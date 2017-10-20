e0910911080e9cfe0f76cb7814c9a4718b5d0df4
andrewparker11 <31515304+andrewparker11@users.noreply.github.com>
2017-10-20 14:42:56 -0400

    Updated from Brython Server: 10/20/2017 2:42:56 PM

--- colorChangeWindow.py
+++ colorChangeWindow.py
@@ -1,1 +1,12 @@
+#Andrew Parker
+#10/20/17
+#colorChangeWindow.py - pops up a window that changes to a random color every time you click it
+
+from random randint 
+
+
+
+
+App().listenMouseEvent('click',mouseClick)
+App().run()
 
