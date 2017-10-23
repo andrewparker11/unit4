5f70bb463c4735be4c19bc0fd89502299abb184d
andrewparker11 <31515304+andrewparker11@users.noreply.github.com>
2017-10-23 14:50:51 -0400

    Updated from Brython Server: 10/23/2017 2:50:50 PM

--- colorChangeWindow.py
+++ colorChangeWindow.py
@@ -1,11 +1,13 @@
 #Andrew Parker
 #10/20/17
 #colorChangeWindow.py - pops up a window that changes to a random color every time you click it
-
+from ggame import *
 from random import randint
 randint (0,1)
 
+blackOutline = LineStyle(1,black) #pixels, color
 
+Rectangle = RectangleAsset(300,50,blackOutline,red) #width, height, outline, fill
 
 
 App().listenMouseEvent('click',mouseClick)
