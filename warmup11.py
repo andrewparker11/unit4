b54c063a3a7f3d786a1e87f1f44e77dea122f1b3
andrewparker11 <31515304+andrewparker11@users.noreply.github.com>
2017-10-23 14:39:57 -0400

    Updated from Brython Server: 10/23/2017 2:39:56 PM

--- warmup11.py
+++ warmup11.py
@@ -3,12 +3,15 @@
 #warmup11.py - determine if a number is prime
 
 def isPrime(n):
-    i = 1
-    if n%i == 0:
-        return True/False  
+    for i in range(2, n-1): 
+        i = 1
+        if n%i == 0:
+            return False  
+    else:
+        return True
     i = i + 1
 
-
+print(isPrime())
 
 
 
