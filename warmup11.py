5a3930265da5f1ee8d575f5895d9e620030e63bb
andrewparker11 <31515304+andrewparker11@users.noreply.github.com>
2017-10-23 14:34:17 -0400

    Updated from Brython Server: 10/23/2017 2:34:16 PM

--- warmup11.py
+++ warmup11.py
@@ -3,17 +3,14 @@
 #warmup11.py - determine if a number is prime
 
 def isPrime(n):
-    y = n
-    i = 2
-    
-    return True/False  
+    i = 1
+    if n%i == 0:
+        return True/False  
+    i = i + 1
 
 
 
-    x = 77 #x is a local variable
-    y = 10 #y is also a local variable
 
-x = 5 
-f() #x does not change
-print(x)
-print(y) #this will cause an error
+
+
+  
