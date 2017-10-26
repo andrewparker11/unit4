b497d8e3b1c29ada9a12b0e3094038ee0607b6db
andrewparker11 <31515304+andrewparker11@users.noreply.github.com>
2017-10-26 14:51:41 -0400

    Updated from Brython Server: 10/26/2017 2:51:41 PM

--- printSquares.py
+++ printSquares.py
@@ -2,17 +2,6 @@
 #10/26/17
 #printSquares.py - prints squares 
 
-"""
-def squares(rows, cols):
-    print('+ - - '*rows+'+')
-    print('|     '*cols+'|')
-    print('+ - - '*rows+'+')
-   
-   
-squares(5,5)"""
-
-
-
 def squares(rows, cols):
     i = 1
     while i<=rows:
