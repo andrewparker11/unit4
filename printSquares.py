30a7970573463aab1b1570af7488906041f7a480
andrewparker11 <31515304+andrewparker11@users.noreply.github.com>
2017-10-26 14:23:03 -0400

    Updated from Brython Server: 10/26/2017 2:23:02 PM

--- printSquares.py
+++ printSquares.py
@@ -1,1 +1,19 @@
+#Andrew Parker
+#10/26/17
+#printSquares.py - prints squares 
 
+def squares(rows, cols):
+    print('+ - - +'*rows)
+   
+   
+   
+def squares(1,1)
+
+
+
+"""
+ if n == 0:
+        print('BOOM!')
+    else:
+        print(n)
+        countdown(n-1)"""
