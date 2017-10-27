965ebb53c18271d7790261f541f4d11502b05142
andrewparker11 <31515304+andrewparker11@users.noreply.github.com>
2017-10-27 14:37:06 -0400

    Updated from Brython Server: 10/27/2017 2:37:05 PM

--- warmup12.py
+++ warmup12.py
@@ -1,1 +1,20 @@
+#Andrew Parker
+#10/27/17
+#warmup12.py - finds GCF
+
+"""
+def GCF(n1,n2):
+    for i in range(2, n-1): 
+        if n%i == 0:
+            return False  
+    return True
+
+print(GCF(10,15))"""
+
+def GCF(n1,n2):
+    for i in range(n1,1,-1): 
+        if n1%i == 0 and n2%i == 0:
+            print(i)
+
+GCF(10,15))
 
