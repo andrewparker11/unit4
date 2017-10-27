88b32aac903edf79f45d0b8a7b4511548babfbb0
andrewparker11 <31515304+andrewparker11@users.noreply.github.com>
2017-10-27 14:46:31 -0400

    Updated from Brython Server: 10/27/2017 2:46:30 PM

--- distance.py
+++ distance.py
@@ -1,3 +1,8 @@
 #Andrew Parker
 #10/26/17
 #distance.py - gives the distane between two points
+
+def distance(x1,y1,x2,y2):
+    answ = sqrt(((x2-x1)**2)+((y2-y1)**2))
+    print(answ)
+distance(1,1,1,1)
