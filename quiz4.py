37769f1eb490d51afed47f2efec6e1410d2953a8
andrewparker11 <31515304+andrewparker11@users.noreply.github.com>
2017-10-30 14:44:07 -0400

    Updated from Brython Server: 10/30/2017 2:44:06 PM

--- quiz4.py
+++ quiz4.py
@@ -7,3 +7,23 @@ def csia(word):
     
 csia('computer science is awesome ')
 
+
+def average(n1,n2,n3):
+    return((n1+n2+n3)/3)
+
+print(average(1,2,3))
+
+
+def lastLetter(string):
+    for ch in string:
+        if ch is
+
+lastLetter('Smedinghoff')
+
+
+def same(a1,a2):
+    if a1==a2:
+        return True
+return False
+    
+print(same(2*3,7-1))
